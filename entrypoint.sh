d778e61fdec6da86a23287b38e68afd9391d6513
Lela810 <leandro.klaus03@gmail.com>
2024-03-22 10:03:42 +0000

    Mar 22, 2024, 11:03 AM

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cd /home/container
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
-
 # Run the Server
-eval ${MODIFIED_STARTUP}
+eval ${STARTUP}
