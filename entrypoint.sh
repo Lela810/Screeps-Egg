a288b95f7c6a9ee8966ea8bf6d2ebbbc496db8f1
Lela810 <leandro.klaus03@gmail.com>
2024-03-25 17:16:54 +0000

    Mar 25, 2024, 6:16 PM

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cd /home/container
 # Run the Server
-eval ${STARTUP}
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+${MODIFIED_STARTUP}
