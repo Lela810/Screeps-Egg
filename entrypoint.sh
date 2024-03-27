a116819bb2c2918f8656db4fd13fa3f27c951b76
Lela810 <leandro.klaus03@gmail.com>
2024-03-27 08:07:12 +0100

    Remove unnecessary code to make internal Docker IP address available to processes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd /home/container
 
-# Make internal Docker IP address available to processes.
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP
-
 # Run the Server
 eval ${STARTUP} 
