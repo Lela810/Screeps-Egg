7750a5e32fb6ef5227b0b0f384ff7ca27d93a30d
Lela810 <leandro.klaus03@gmail.com>
2024-03-27 10:01:07 +0100

    Add Docker image build and push actions, and sign the published Docker image

---
+++ Screeps-Steamless-Client/install.ps1
@@ -0,0 +1,3 @@
+winget install nodejs
+npm i -g screepers-steamless-client
+Copy-Item -Path "Screeps Web Client.lnk" -Destination "C:\ProgramData\Microsoft\Windows\Start Menu\Screeps Web Client.lnk"
